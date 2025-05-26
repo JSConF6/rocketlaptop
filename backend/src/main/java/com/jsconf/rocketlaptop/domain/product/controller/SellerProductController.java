e69a00fa4000e0fb5691ed906afaaed9b3dcac86
JSConF <arare30030@naver.com>
2025-05-26 12:04:07 +0900

    Feat: Seller Controller, Service 추가

--- backend/src/main/java/com/jsconf/rocketlaptop/domain/product/controller/SellerProductController.java
+++ backend/src/main/java/com/jsconf/rocketlaptop/domain/product/controller/SellerProductController.java
@@ -1,19 +1,24 @@
 package com.jsconf.rocketlaptop.domain.product.controller;
 
-import com.jsconf.rocketlaptop.domain.product.service.ProductService;
+import com.jsconf.rocketlaptop.common.SuccessCode;
+import com.jsconf.rocketlaptop.domain.member.model.Member;
+import com.jsconf.rocketlaptop.domain.product.service.SellerProductService;
+import com.jsconf.rocketlaptop.security.annotation.LoginMember;
+import com.jsconf.rocketlaptop.util.ResponseUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/product")
-public class ProductController {
-    private final ProductService productService;
+@RequestMapping("/api/seller/products")
+public class SellerProductController {
+    private final SellerProductService productService;
 
-    @GetMapping()
-    public void getSellerProducts() {
-
+    @GetMapping
+    public ResponseEntity<?> getSellerProducts(@LoginMember Member member) {
+        return ResponseUtil.success(SuccessCode.SUCCESS);
     }
 }
--- backend/src/main/java/com/jsconf/rocketlaptop/domain/product/service/SellerProductService.java
+++ backend/src/main/java/com/jsconf/rocketlaptop/domain/product/service/SellerProductService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class ProductService {
+public class SellerProductService {
     private final ProductMapper productMapper;
 
     public void findProductsBySeller() {
